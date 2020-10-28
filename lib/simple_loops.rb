0a2e5bf76e1b461f080cc3fa7d2d497faeb14263
Andres Hernandez <andres.hernandez@utexas.edu>
2020-10-28 18:44:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,10 @@
 # Write your methods here
-def loop_message_five_times
+def loop_message_five_times(string)
+ puts string
+ puts string
+ puts string
+ puts string
+ puts string
 end
 
 def loop_message_n_times
